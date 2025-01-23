7bd6cf210f9485ec161003a892514bc74c033ea0
Himanshu0721 <himanshusinghrajput.0921@gmail.com>
2025-01-23 20:55:43 +0530

    commited

--- Frontend/src/Components/Login/Register.jsx
+++ Frontend/src/Components/Login/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
       password,
     };
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://everything-engine-cdek.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- Frontend/src/Pages/Feedback/FeedBack.jsx
+++ Frontend/src/Pages/Feedback/FeedBack.jsx
@@ -16,7 +16,7 @@ const FeedBack = () => {
     };
     console.log(requestData);
     try {
-      const response = await fetch("http://localhost:5000/api/feedback", {
+      const response = await fetch("https://everything-engine-cdek.onrender.com/api/feedback", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
