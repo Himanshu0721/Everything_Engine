3e29e7472b2ca10d56706ad2afbcba3f4bfc410c
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2025-01-23 11:26:42 +0530

    profile

--- Frontend/src/Pages/Profile/ProfileModal.jsx
+++ Frontend/src/Pages/Profile/ProfileModal.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "../../Components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
 import { X } from "lucide-react";
 import "./profileModal.css";
-import { UserContext } from "@/Context/userContext";
+import { UserContext } from "../../Context/userContext";
 
 const ProfileModal = ({
   closeProfileModal,
